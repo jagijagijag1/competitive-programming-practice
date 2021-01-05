c593f0d96d6541dc5dbadd8606b23617936fa932
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-01-05 10:19:54 +0900

    💪

---
+++ AtCoder/ARC/099/C/ARC099C.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	N, K := nextInt(), nextInt()
+	fmt.Println(math.Ceil(float64(N-1) / float64(K-1)))
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
