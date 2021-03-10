ed6b76dc1a74aadae64f88aa0bf0033b26951455
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-03-10 20:20:27 +0900

    💪

---
+++ AtCoder/ABC/160/D/ABC160D.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	N, X, Y := nextInt(), nextInt(), nextInt()
+
+	res := make([]int, N)
+	for i := 1; i < N; i++ {
+		for j := i + 1; j <= N; j++ {
+			d := min(j-i, min(abs(X-i)+1+abs(j-Y), abs(Y-i)+1+abs(j-X)))
+			res[d]++
+		}
+	}
+
+	for i := 1; i < N; i++ {
+		fmt.Println(res[i])
+	}
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
