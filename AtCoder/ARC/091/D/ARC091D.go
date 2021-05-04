ef4eebac815d0665e0458d821130cabc1f0969bb
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-05-04 10:19:42 +0900

    💪

---
+++ AtCoder/ARC/091/D/ARC091D.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	N, K, res := nextInt(), nextInt(), 0
+
+	for b := 1; b <= N; b++ {
+		p, r := N/b, N%b
+		res += p*max(0, b-K) + max(0, r-K+1)
+	}
+	if K == 0 {
+		res -= N
+	}
+	fmt.Println(res)
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
