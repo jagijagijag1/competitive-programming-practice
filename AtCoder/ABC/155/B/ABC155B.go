8376e0305342fc651e04ef0c2cd8616c03d9b933
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-06-17 20:53:12 +0900

    💪

--- AtCoder/ABC/155/B/ABC155B.go
+++ AtCoder/ABC/155/B/ABC155B.go
@@ -9,38 +9,17 @@ import (
 
 func main() {
 	sc.Split(bufio.ScanWords)
-	n, m := nextInt(), nextInt()
-	ss := make([][]int, m)
-	for i := 0; i < m; i++ {
-		k := nextInt()
-		for j := 0; j < k; j++ {
-			ss[i] = append(ss[i], nextInt()-1)
-		}
-	}
-	p := make([]int, m)
-	for i := 0; i < m; i++ {
-		p[i] = nextInt()
-	}
-
-	res := 0
-	for bit := 0; bit < (1 << uint(n)); bit++ {
-		on := 0
-		for i := 0; i < m; i++ {
-			s_on := 0
-			for j := range ss[i] {
-				if bit>>uint(ss[i][j])&1 == 1 {
-					s_on++
-				}
-			}
-			if s_on%2 == p[i] {
-				on++
+	n := nextInt()
+	for i := 0; i < n; i++ {
+		a := nextInt()
+		if a%2 == 0 {
+			if a%3 != 0 && a%5 != 0 {
+				fmt.Println("DENIED")
+				return
 			}
 		}
-		if on == m {
-			res++
-		}
 	}
-	fmt.Println(res)
+	fmt.Println("APPROVED")
 }
 
 var sc = bufio.NewScanner((os.Stdin))
