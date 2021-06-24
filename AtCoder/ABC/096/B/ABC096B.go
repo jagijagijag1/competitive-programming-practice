2119df86cec28b458643f31ccc8b0aacd02f58b1
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-06-24 19:36:26 +0900

    💪

---
+++ AtCoder/ABC/096/B/ABC096B.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	a, b, c, k := nextInt(), nextInt(), nextInt(), nextInt()
+	m := max(a, max(b, c))
+
+	res := 0
+	if m == a {
+		res = b + c
+	} else if m == b {
+		res = c + a
+	} else {
+		res = a + b
+	}
+
+	for i := 0; i < k; i++ {
+		m *= 2
+	}
+	fmt.Println(res + m)
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
