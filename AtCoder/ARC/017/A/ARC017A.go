c093aa4be7a5f223b1626163bd7a349858b36da9
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-06-26 10:21:51 +0900

    💪

---
+++ AtCoder/ARC/017/A/ARC017A.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	fmt.Println(isPrime(nextInt()))
+}
+
+func isPrime(n int) string {
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return "NO"
+		}
+	}
+	return "YES"
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
