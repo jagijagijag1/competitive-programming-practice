f3ca910c8163937af5a872ae3500ad98effec23a
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-07-13 20:09:24 +0900

    💪

---
+++ AtCoder/Other/CodeFormula2014qualA/A/CodeFormula2014qualAA.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	a := nextInt()
+	for i := 1; i*i*i <= 1000000; i++ {
+		if i*i*i == a {
+			fmt.Println("YES")
+			return
+		}
+	}
+	fmt.Println("NO")
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
