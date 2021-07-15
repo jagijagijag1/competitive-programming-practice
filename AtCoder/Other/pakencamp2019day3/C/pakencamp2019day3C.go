d5961d8013a8abd92c14d69ba72e7a4a473fdef0
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-07-15 22:15:57 +0900

    💪

--- AtCoder/Other/pakencamp2019day3/C/pakencamp2019day3C.go
+++ AtCoder/Other/pakencamp2019day3/C/pakencamp2019day3C.go
@@ -9,7 +9,33 @@ import (
 
 func main() {
 	sc.Split(bufio.ScanWords)
-	fmt.Println(9.0/5.0*float64(nextInt()) + 32.0)
+	n, m := nextInt(), nextInt()
+	a := make([][]int, n)
+	for i := range a {
+		a[i] = make([]int, m)
+		for j := range a[i] {
+			a[i][j] = nextInt()
+		}
+	}
+
+	res := 0
+	for t1 := 0; t1 < m-1; t1++ {
+		for t2 := t1 + 1; t2 < m; t2++ {
+			tmp := 0
+			for nn := 0; nn < n; nn++ {
+				tmp += max(a[nn][t1], a[nn][t2])
+			}
+			res = max(res, tmp)
+		}
+	}
+	fmt.Println(res)
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
 }
 
 var sc = bufio.NewScanner((os.Stdin))
