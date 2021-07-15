51482c539c888167292c500da5651d00e05692ef
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-07-15 20:27:14 +0900

    💪

--- AtCoder/ABC/054/C/ABC054C.go
+++ AtCoder/ABC/054/C/ABC054C.go
@@ -7,6 +7,90 @@ import (
 	"strconv"
 )
 
+func main() {
+	sc.Split(bufio.ScanWords)
+	n, m := nextInt(), nextInt()
+	e := make([][]int, n)
+	for i := range e {
+		e[i] = []int{}
+	}
+	for i := 0; i < m; i++ {
+		a, b := nextInt()-1, nextInt()-1
+		e[a] = append(e[a], b)
+		e[b] = append(e[b], a)
+	}
+
+	res := 0
+	permus := permutations(n)
+	for _, p := range permus {
+		if p[0] != 0 {
+			continue
+		}
+		flag := true
+		for i := 1; i < len(p); i++ {
+			if !contains(e[p[i-1]], p[i]) {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			res++
+		}
+	}
+	fmt.Println(res)
+}
+
+func contains(s []int, e int) bool {
+	for _, v := range s {
+		if e == v {
+			return true
+		}
+	}
+	return false
+}
+
+func permutations(n int) [][]int {
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		arr[i] = i
+	}
+
+	var helper func([]int, int)
+	res := [][]int{}
+
+	helper = func(arr []int, n int) {
+		if n == 1 {
+			tmp := make([]int, len(arr))
+			copy(tmp, arr)
+			res = append(res, tmp)
+		} else {
+			for i := 0; i < n; i++ {
+				helper(arr, n-1)
+				if n%2 == 1 {
+					tmp := arr[i]
+					arr[i] = arr[n-1]
+					arr[n-1] = tmp
+				} else {
+					tmp := arr[0]
+					arr[0] = arr[n-1]
+					arr[n-1] = tmp
+				}
+			}
+		}
+	}
+	helper(arr, len(arr))
+	return res
+}
+
+func isPrime(n int) bool {
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
 var sc = bufio.NewScanner((os.Stdin))
 
 func nextLine() string {
@@ -23,53 +107,16 @@ func nextInt() int {
 	return i
 }
 
-// func main() {
-func mainABC054C() {
-	sc.Split(bufio.ScanWords)
-	N, M, a, b := nextInt(), nextInt(), []int{}, []int{}
-	for i := 0; i < M; i++ {
-		a = append(a, nextInt())
-		b = append(b, nextInt())
-	}
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
 
-	fmt.Printf("%d\n", ABC054C(N, a, b))
-}
-
-// ABC054C ...
-func ABC054C(N int, a, b []int) (res int) {
-	graph := []GraphNode{}
-	for i := 0; i < N; i++ {
-		graph = append(graph, GraphNode{i, []int{}})
-	}
-	for i := 0; i < len(a); i++ {
-		graph[a[i]-1].edges = append(graph[a[i]-1].edges, b[i]-1)
-		graph[b[i]-1].edges = append(graph[b[i]-1].edges, a[i]-1)
-	}
-
-	return dfsABC054(graph[0], graph, []int{}, 0)
-}
-
-func dfsABC054(current GraphNode, graph []GraphNode, path []int, res int) int {
-	for _, n := range path {
-		if n == current.id {
-			return res
-		}
-	}
-
-	path = append(path, current.id)
-	if len(path) == len(graph) {
-		return res + 1
-	}
-
-	for _, e := range current.edges {
-		res = dfsABC054(graph[e], graph, path, res)
-	}
-
-	return res
-}
-
-// GraphNode ...
-type GraphNode struct {
-	id    int
-	edges []int
-}
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
---
+++ AtCoder/ARC/015/A/ARC015A.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	fmt.Println(9.0/5.0*float64(nextInt()) + 32.0)
+}
+
+func isPrime(n int) bool {
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
