f8565dd5a15ac36c568ad525ad5c0df1739dc33a
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-08-05 20:03:04 +0900

    💪

---
+++ AtCoder/ABC/153/C/ABC153C.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	n, k, h := nextInt(), nextInt(), []int{}
+	for i := 0; i < n; i++ {
+		h = append(h, nextInt())
+	}
+	sort.Ints(h)
+
+	res := 0
+	for i := n - 1 - k; 0 <= i; i-- {
+		res += h[i]
+	}
+	fmt.Println(res)
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
---
+++ AtCoder/Other/Keyence2020/A/Keyence2020A.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	h, w, n := nextInt(), nextInt(), nextInt()
+	if h < w {
+		fmt.Println(int(math.Ceil(float64(n) / float64(w))))
+	} else {
+		fmt.Println(int(math.Ceil(float64(n) / float64(h))))
+	}
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
