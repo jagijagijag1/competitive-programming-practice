6e8cf259419561e3890e7f3579134003f11e37d0
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-08-10 09:43:05 +0900

    💪

---
+++ AtCoder/Other/HHKB2020/C/HHKB2020C.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	n, used, m := nextInt(), make([]bool, 200001), 0
+	for i := 0; i < n; i++ {
+		p := nextInt()
+		used[p] = true
+		if p == m {
+			for j := m + 1; ; j++ {
+				if used[j] == false {
+					m = j
+					break
+				}
+			}
+		}
+		fmt.Println(m)
+	}
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
