bd69c579787e56558850542d852f0b9769fdac30
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-09-09 19:39:59 +0900

    💪

---
+++ AtCoder/Other/m_solutions2020/B/m_solutions2020B.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	a, b, c, k := nextInt(), nextInt(), nextInt(), nextInt()
+
+	for i := 0; i < k; i++ {
+		if b <= a {
+			b *= 2
+		} else if c <= b {
+			c *= 2
+		} else {
+			break
+		}
+	}
+
+	if a < b && b < c {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
