66823aaa362f261d364732e9fc7377e244628791
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-09-13 19:31:57 +0900

    💪

---
+++ AtCoder/Other/aising2020/A/aising2020A.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	l, r, d, res := nextInt(), nextInt(), nextInt(), 0
+
+	for i := l; i <= r; i++ {
+		if i%d == 0 {
+			res++
+		}
+	}
+	fmt.Println(res)
+
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
