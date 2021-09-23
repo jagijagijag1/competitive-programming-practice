e1bb5d424c64345a8e16933f77d646321c3b61d3
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-09-23 11:41:58 +0900

    💪

---
+++ AtCoder/Other/nomura2020/A/nomura2020A.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	h1, m1, h2, m2, k := nextInt(), nextInt(), nextInt(), nextInt(), nextInt()
+	fmt.Println((h2-h1)*60 + (m2 - m1) - k)
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
