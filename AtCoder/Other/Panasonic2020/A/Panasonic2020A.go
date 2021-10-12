40b15940914682b23ef9b40b5f1d951415c1a558
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-10-12 19:53:57 +0900

    💪

---
+++ AtCoder/Other/Panasonic2020/A/Panasonic2020A.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	a := [32]int{1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51}
+	fmt.Println(a[nextInt()-1])
+}
+
+var sc = bufio.NewScanner((os.Stdin))
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// const (
+// 	initialBufSize = 10000
+// 	maxBufSize     = 1000000
+// )
+
+// var (
+// 	sc *bufio.Scanner = func() *bufio.Scanner {
+// 		sc := bufio.NewScanner(os.Stdin)
+// 		buf := make([]byte, initialBufSize)
+// 		sc.Buffer(buf, maxBufSize)
+// 		return sc
+// 	}()
+// )
