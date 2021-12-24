688ade96231c1f2287919cb8fb8eaee735605a3d
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2021-12-24 19:50:45 +0900

    💪

---
+++ AtCoder/ABC/220/A/ABC220A.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	a, b, c := nextInt(), nextInt(), nextInt()
+	for i := a; i <= b; i++ {
+		if i%c == 0 {
+			fmt.Println(i)
+			return
+		}
+	}
+	fmt.Println(-1)
+}
+
+const (
+	initialBufSize = 10000
+	maxBufSize     = 1000000
+)
+
+var (
+	sc *bufio.Scanner = func() *bufio.Scanner {
+		sc := bufio.NewScanner(os.Stdin)
+		buf := make([]byte, initialBufSize)
+		sc.Buffer(buf, maxBufSize)
+		return sc
+	}()
+)
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	l := nextLine()
+	i, e := strconv.Atoi(l)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+// var sc = bufio.NewScanner((os.Stdin))
