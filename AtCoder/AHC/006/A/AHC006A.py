d00bed77a1572102e8256086405a19106fe74a91
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2022-06-13 21:27:30 +0900

    💪 AHC006A greedy1

--- AtCoder/AHC/006/A/AHC006A.py
+++ AtCoder/AHC/006/A/AHC006A.py
@@ -24,17 +24,80 @@ def read_input():
 
 def solve(rest_x, rest_y, dest_x, dest_y):
     """
-    0, 1, 2, ..., 49番目のレストラン → 0, 1, 2, ..., 49番目の配達先を順に回る
+    今いる場所に最も近いレストランを50個貪欲に回り、
+    その後最も近い配達先を貪欲に回る
     """
-    orders = list(range(PICKUP_COUNT))
+    orders = []
     result_x = [CENTER]
     result_y = [CENTER]
-    result_x.extend(rest_x[:PICKUP_COUNT])
-    result_x.extend(dest_x[:PICKUP_COUNT])
-    result_y.extend(rest_y[:PICKUP_COUNT])
-    result_y.extend(dest_y[:PICKUP_COUNT])
+
+    # 採用する注文の候補（1～1000番の全てを候補とする）
+    candidates = list(range(ORDER_COUNT))
+
+    # 現在位置をオフィスにセット
+    current_x = CENTER
+    current_y = CENTER
+
+    # 今いる場所に最も近いレストランを50個貪欲に回る
+    for _ in range(PICKUP_COUNT):
+        # 候補の中から、一番近いレストランを探す
+        min_dist = 1000000007
+        min_index = 998244353
+
+        for i, v in enumerate(candidates):
+            d = dist(current_x, current_y, rest_x[v], rest_y[v])
+            if d < min_dist:
+                min_dist = d
+                min_index = i
+
+        # レストランの注文番号
+        v = candidates[min_index]
+
+        # 解を更新
+        orders.append(v)
+        result_x.append(rest_x[v])
+        result_y.append(rest_y[v])
+
+        # 現在位置を更新
+        current_x = rest_x[v]
+        current_y = rest_y[v]
+
+        # 既に到着したレストランは候補から削除
+        candidates.pop(min_index)
+
+    # 行かなきゃいけない配達先（＝これまでに行ったレストラン）のリスト
+    candidates = orders.copy()
+
+    # 今いる場所に最も近い配達先を貪欲に回る
+    for _ in range(PICKUP_COUNT):
+        # まだ行っていない配達先の中から、一番近い配達先を探す
+        min_dist = 1000000007
+        min_index = 998244353
+
+        for i, v in enumerate(candidates):
+            d = dist(current_x, current_y, dest_x[v], dest_y[v])
+            if d < min_dist:
+                min_dist = d
+                min_index = i
+
+        # 配達先の注文番号
+        v = candidates[min_index]
+
+        # 解を更新
+        result_x.append(dest_x[v])
+        result_y.append(dest_y[v])
+
+        # 現在位置を更新
+        current_x = dest_x[v]
+        current_y = dest_y[v]
+
+        # 既に到着した配達先は候補から削除
+        candidates.pop(min_index)
+
+    # 最後にオフィスに戻る
     result_x.append(CENTER)
     result_y.append(CENTER)
+
     return orders, result_x, result_y
 
 
