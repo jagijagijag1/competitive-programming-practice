10cfc74c5cce3fd989ac19f98d990814a143456d
jagijagijag1 <29348109+jagijagijag1@users.noreply.github.com>
2022-10-01 10:35:36 +0900

    ADD: next permutation

--- AtCoder/utils.go
+++ AtCoder/utils.go
@@ -137,6 +137,37 @@ func permutationsStr(arr []string) [][]string {
 	return res
 }
 
+// https://go.dev/play/p/ljft9xhOEn
+// NextPermutation generates the next permutation of the
+// sortable collection x in lexical order.  It returns false
+// if the permutations are exhausted.
+//
+// Knuth, Donald (2011), "Section 7.2.1.2: Generating All Permutations",
+// The Art of Computer Programming, volume 4A.
+func NextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
 func min(a, b int) int {
 	if a > b {
 		return b
